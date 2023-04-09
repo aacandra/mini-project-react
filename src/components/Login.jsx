8d57f732d248cfab606eac8aeccd5f4da75f61c3
acandra <aacandra@gmail.com>
2023-04-09 11:07:09 +0700

    add redirect files

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,34 +1,66 @@
-import React from 'react'
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import "../style.css"
-
+import React from 'react';
+import "../style.css";
+import { useFormik } from 'formik';
+import axios from 'axios';
 
 const Login = () => {
-  return (
-    <div className='formLogin'>
-        <Form className='form-main'>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
-      </Form.Group>
+  const formik = useFormik({
+    initialValues: {
+      username: '',
+      password: ''
+    },
+    onSubmit: values => {
+      const username = values.username;
+      const password = values.password;
+      axios({
+        method: 'post',
+        url: 'https://api.themoviedb.org/3/authentication/token/new?api_key=1e05d6c6fe757614cf08f4083d927aaf',
+        data: {
+          username: username,
+          password: password
+        }
+      })
+      .then(function (response) {
+        const token = response.data.request_token;
+        axios({
+          method: 'post',
+          url: 'https://api.themoviedb.org/3/authentication/session/new?api_key=1e05d6c6fe757614cf08f4083d927aaf',
+          data: {
+            request_token: token
+          }
+        })
+        .then(function (response) {
+          console.log(response.data.session_id);
+        });
+      });
+    },
+  });
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
-    </div>
-  )
+  return (
+    <form className='login-form' onSubmit={formik.handleSubmit}>
+      <div>
+        <label htmlFor="username">Username</label>
+        <input className='input-form'
+          id="username"
+          name="username"
+          type="text"
+          onChange={formik.handleChange}
+          value={formik.values.username}
+        />
+      </div>
+      <div>
+        <label htmlFor="password">Password</label>
+        <input className='input-form'
+          id="password"
+          name="password"
+          type="password"
+          onChange={formik.handleChange}
+          value={formik.values.password}
+        />
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  );
 }
 
-export default Login
+export default Login;
